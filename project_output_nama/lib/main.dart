17ae043c548fa1900380b1a933da34892f2125ab
Ikhwan049 <ikhwanmuhammad2121@gmail.com>
2022-04-06 13:31:16 +0800

    project

---
+++ project_output_nama/lib/main.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Center(
+      child: Text(
+          "Halo nama saya MUHAMMAD IKHWANUL, biasa dipanggil WANN",
+          textDirection: TextDirection.ltr
+      ),
+    )
+  );
+}
